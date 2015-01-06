32f500c667642d8afabdee5753e25977f20ca4d7
朱永光 <zyg@ike-global.com>
2015-01-06 11:10:39 +0800

    初步搭建项目，实现QBWI发送。

---
+++ MailSender/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MailSender.MvcApplication" Language="C#" %>
---
+++ MailSender/Models/SendViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MailSender.Models
+{
+    public class SendViewModel
+    {
+        [Display(Name = "发送人")]
+        public string FromName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "发送地址")]
+        public string FromEmail { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "接收地址")]
+        public string ToEmail { get; set; }
+
+        [Required]
+        [Display(Name = "主题")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "内容")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// 0:QBWI,1:Hangfire,2:WebJobs
+        /// </summary>
+        public int SendBy { get; set; }
+    }
+}

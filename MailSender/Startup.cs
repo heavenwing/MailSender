e1d05ccf1e278c2b93c0210543bdebb91b964998
朱永光 <zyg@ike-global.com>
2015-01-31 08:57:01 +0800

    添加hangfire的授权控制扩展包。

--- MailSender/Startup.cs
+++ MailSender/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Hangfire;
+using Hangfire.Dashboard;
 using Hangfire.SqlServer;
 using Microsoft.Owin;
 using Owin;
@@ -16,6 +17,17 @@ namespace MailSender
             {
                 config.UseSqlServerStorage("Hangfire");
                 config.UseServer();
+
+                //config.UseAuthorizationFilters(new AuthorizationFilter
+                //{
+                //    Users = "admin, superuser", // allow only specified users
+                //    Roles = "admins" // allow only specified roles
+                //});
+
+                //// or
+
+                //config.UseAuthorizationFilters(
+                //    new ClaimsBasedAuthorizationFilter("hangfire", "access"));
             });
 
             app.MapSignalR();
